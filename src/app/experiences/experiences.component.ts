f6506eed8410697d7dd2d350236620232dfb14e9
Gustavo <gustavohdlima@hotmail.com>
2025-07-25 20:49:53 -0300

    refactor: removing experience

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -27,14 +27,5 @@ export class ExperiencesComponent {
       technologies: ['.NET', 'C#', 'Angular', 'Typescript'],
       side: 'right',
     },
-    {
-      role: $localize`Desenvolvedor Júnior`,
-      period: $localize`Mar 2022 – Ago 2022`,
-      company: $localize`Luz - Soluções Financeiras`,
-      description: $localize`Na Luz pude atuar tanto no backend quanto no frontend (com foco no frontend) onde trabalhei
-      tecnologias modernas na construção da calculadora de renda fixa na nuvem da empresa.`,
-      technologies: ['Angular', 'Jasmine', 'Selenium'],
-      side: 'left',
-    },
   ];
 }
